d95e7a516084cf92f6d3d01b488af1fc16ee2787
chinaxxren <49141798+chinaxxren@users.noreply.github.com>
2019-08-15 14:13:38 +0800

    ScrollController

---
+++ scroll/flutter_scroll_controller/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/jiangmingz/Bak/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jiangmingz/Projects/Flutter/FlutterChina/scroll/flutter_scroll_controller"
+export "FLUTTER_TARGET=/Users/jiangmingz/Projects/Flutter/FlutterChina/scroll/flutter_scroll_controller/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/jiangmingz/Bak/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
---
+++ scroll/flutter_scroll_controller/lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ScrollControllerPage(),
+    );
+  }
+}
+
+class ScrollControllerPage extends StatefulWidget {
+  @override
+  ScrollControllerPageState createState() {
+    return new ScrollControllerPageState();
+  }
+}
+
+class ScrollControllerPageState extends State<ScrollControllerPage> {
+  ScrollController _controller = new ScrollController();
+  bool showToTopBtn = false; //是否显示“返回到顶部”按钮
+
+  @override
+  void initState() {
+    //监听滚动事件，打印滚动位置
+    _controller.addListener(() {
+      //打印滚动位置
+      print(_controller.offset);
+      if (_controller.offset < 1000 && showToTopBtn) {
+        setState(() {
+          showToTopBtn = false;
+        });
+      } else if (_controller.offset >= 1000 && showToTopBtn == false) {
+        setState(() {
+          showToTopBtn = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    //为了避免内存泄露，需要调用_controller.dispose
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("滚动控制")),
+      body: Scrollbar(
+        child: ListView.builder(
+            itemCount: 100,
+            itemExtent: 50.0, //列表项高度固定时，显式指定高度是一个好习惯(性能消耗小)
+            controller: _controller,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text("$index"),
+              );
+            }),
+      ),
+      floatingActionButton: !showToTopBtn
+          ? null
+          : FloatingActionButton(
+              child: Icon(Icons.arrow_upward),
+              onPressed: () {
+                //返回到顶部时执行动画
+                _controller.animateTo(.0, duration: Duration(milliseconds: 200), curve: Curves.ease);
+              }),
+    );
+  }
+}
