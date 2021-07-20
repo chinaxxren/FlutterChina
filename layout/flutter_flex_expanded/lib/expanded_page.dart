67bf80569e08244fcf0ac3980f4790a21976daea
chinaxxren <jiangmingz@qq.com>
2021-07-21 00:27:05 +0800

    flex and explanded spacer

---
+++ layout/flutter_flex_expanded/lib/expanded_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class ExpandedPage extends StatefulWidget {
+  ExpandedPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _ExpandedPageState createState() => _ExpandedPageState();
+}
+
+class _ExpandedPageState extends State<ExpandedPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Flex(
+              direction: Axis.horizontal,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 30,
+                    color: Colors.red,
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 30,
+                    color: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: SizedBox(
+                height: 100.0,
+                child: Flex(
+                  direction: Axis.vertical,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        height: 30,
+                        color: Colors.red,
+                      ),
+                    ),
+                    Spacer(
+                      flex: 1,
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        height: 30,
+                        color: Colors.green,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- layout/flutter_flex_expanded/lib/main.dart
+++ layout/flutter_flex_expanded/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_flex_expanded/expanded_demo.dart';
+import 'expanded_page.dart';
+import 'flexible_demo.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,80 +13,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Flex Expanded'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Flex(
-              direction: Axis.horizontal,
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    height: 30,
-                    color: Colors.red,
-                  ),
-                ),
-                Expanded(
-                  flex: 2,
-                  child: Container(
-                    height: 30,
-                    color: Colors.green,
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child: SizedBox(
-                height: 100.0,
-                child: Flex(
-                  direction: Axis.vertical,
-                  children: <Widget>[
-                    Expanded(
-                      flex: 2,
-                      child: Container(
-                        height: 30,
-                        color: Colors.red,
-                      ),
-                    ),
-                    Spacer(
-                      flex: 1,
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        height: 30,
-                        color: Colors.green,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      // home: ExpandedPage(title: 'Flutter Flex Expanded'),
+      home: ExpandedDemo(),
+      // home: FlexibeDemo(),
     );
   }
 }
