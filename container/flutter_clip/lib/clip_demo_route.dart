5bc35f9757348520572db7b9d6cced9cc362bdbf
chinaxxren <jiangmingz@qq.com>
2021-07-23 02:36:25 +0800

    clipoval

--- container/flutter_clip/lib/clip_demo_route.dart
+++ container/flutter_clip/lib/clip_demo_route.dart
@@ -10,7 +10,7 @@ class ClipDemoRoute extends StatefulWidget {
 class _ClipDemoRouteState extends State {
   @override
   Widget build(BuildContext context) {
-    Image localImage = Image.asset('images/avatar.png', fit: BoxFit.fill);
+    Image localImage = Image.asset('images/task.png', fit: BoxFit.fill);
     Image netImage = Image.network('https://images.unsplash.com/photo-1600173300839-55c4e3ed367b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80', fit: BoxFit.fill);
     return Scaffold(
       appBar: AppBar(
@@ -23,8 +23,8 @@ class _ClipDemoRouteState extends State {
             width: 140,
             height: 140,
             color: Colors.green,
-            // child: localImage,
-            child: netImage,
+            child: localImage,
+            // child: netImage,
           ),
         ),
       ),
